fcf61017443a7b540e9e739f5c7a0064c9395850
Alexandre Chaussier <a.chaussier@infopen.pro>
2016-11-23 17:10:47 +0100

    Add package management from MongoDB download center

--- tests/testinfra/test_installation.py
+++ tests/testinfra/test_installation.py
@@ -9,8 +9,15 @@ pytestmark = pytest.mark.docker_images(
     'infopen/ubuntu-xenial-ssh-py27:0.2.0'
 )
 
-def test_foo_a(User):
-    assert User().name == 'root'
 
-def test_foo_b(User):
-    assert User().name == 'root'
+def test_temporary_package_file(SystemInfo, File):
+    """
+    Test downloaded package from URL
+    """
+
+    if SystemInfo.distribution == 'ubuntu':
+        filename = '/tmp/mongodb-mms_2.0.7.372-1_x86_64.deb'
+
+    package = File(filename)
+    assert package.exists
+    assert package.is_file
