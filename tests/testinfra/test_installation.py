ece6e415fc4cb4ebbb551faa8d1c32e77ca63eef
Alexandre Chaussier <a.chaussier@infopen.pro>
2016-12-02 00:23:53 +0100

    Remove Trusty docker testing due to a problem with MongoDB service

--- tests/testinfra/test_installation.py
+++ tests/testinfra/test_installation.py
@@ -5,7 +5,6 @@ import pytest
 
 # To run all the tests on given docker images:
 pytestmark = pytest.mark.docker_images(
-    'infopen/ubuntu-trusty-ssh:0.1.0',
     'infopen/ubuntu-xenial-ssh-py27:0.2.0'
 )
 
